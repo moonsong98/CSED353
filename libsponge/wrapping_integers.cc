f61ce26e78d9483b236b1a0a6dfc5d7672610a93
Moon Kyung Song <moonsong98@postech.ac.kr>
2023-03-17 07:04:54 -0700

    Lab2: Complete Wrapping Integers

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -1,5 +1,7 @@
 #include "wrapping_integers.hh"
 
+#include <iostream>
+
 // Dummy implementation of a 32-bit wrapping integer
 
 // For Lab 2, please replace with a real implementation that passes the
@@ -10,12 +12,15 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
+const uint64_t uint32_bits = 1ul << 32;
+
 //! Transform an "absolute" 64-bit sequence number (zero-indexed) into a WrappingInt32
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
+    uint64_t isn_raw = isn.raw_value();
+    uint32_t mod_result = ((n % uint32_bits) + (isn_raw % uint32_bits)) % uint32_bits;
+    return WrappingInt32{mod_result};
 }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
@@ -29,6 +34,8 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    // Calculate the offset from checkpoint to n, then add it to checkpoint
+    // Exception: Since index of sequence starts with 0, add uint32_bits if checkpoint + diff is less than 0
+    int64_t diff = n - wrap(checkpoint, isn);
+    return checkpoint + diff + (static_cast<int64_t>(checkpoint) + diff < 0 ? uint32_bits : 0);
 }
