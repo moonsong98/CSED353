1e4f65bc542fcb62f2b1721f074d0ecc33ab7e5f
Moon Kyung Song <moonsong98@postech.ac.kr>
2023-03-17 08:17:28 -0700

    Lab2: Passed All Tests

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -1,4 +1,5 @@
 #include "tcp_receiver.hh"
+#include <iostream>
 
 // Dummy implementation of a TCP receiver
 
@@ -11,9 +12,23 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 using namespace std;
 
 void TCPReceiver::segment_received(const TCPSegment &seg) {
-    DUMMY_CODE(seg);
+    seg.payload();
+    if(!recievedSYN && !seg.header().syn) return;
+    if(seg.header().syn) {
+        isn = WrappingInt32(seg.header().seqno);
+        recievedSYN = true;
+    }
+    if(seg.header().fin)
+        recievedFIN = true;
+    uint64_t abs_seqno = unwrap(seg.header().seqno, isn, _reassembler.num_written_bytes());
+    _reassembler.push_substring(seg.payload().copy(), abs_seqno - (seg.header().syn ? 0 : 1), seg.header().fin);
 }
 
-optional<WrappingInt32> TCPReceiver::ackno() const { return {}; }
+optional<WrappingInt32> TCPReceiver::ackno() const { 
+    if(recievedSYN) return WrappingInt32(isn + _reassembler.num_written_bytes() + recievedSYN + (_reassembler.empty()?recievedFIN:0)); 
+    return nullopt;
+}
 
-size_t TCPReceiver::window_size() const { return {}; }
+size_t TCPReceiver::window_size() const { 
+    return _reassembler.remaining_capacity();
+}
--- libsponge/tcp_receiver.hh
+++ libsponge/tcp_receiver.hh
@@ -20,12 +20,17 @@ class TCPReceiver {
     //! The maximum number of bytes we'll store.
     size_t _capacity;
 
+    // Initial sequence Number
+    WrappingInt32 isn;
+    bool recievedSYN = false;
+    bool recievedFIN = false;
+    
   public:
     //! \brief Construct a TCP receiver
     //!
     //! \param capacity the maximum number of bytes that the receiver will
     //!                 store in its buffers at any give time.
-    TCPReceiver(const size_t capacity) : _reassembler(capacity), _capacity(capacity) {}
+    TCPReceiver(const size_t capacity) : _reassembler(capacity), _capacity(capacity), isn(0) {}
 
     //! \name Accessors to provide feedback to the remote TCPSender
     //!@{
