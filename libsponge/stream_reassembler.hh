621f82cf8b659050030ae0d7b386c1dc034a82d1
Moon Kyung Song <moonsong98@postech.ac.kr>
2023-03-17 07:06:01 -0700

    Lab2: Add Public Method at StreamReassembler to Retrieve Last Reassembled Byte

--- libsponge/stream_reassembler.hh
+++ libsponge/stream_reassembler.hh
@@ -64,6 +64,8 @@ class StreamReassembler {
     //! \brief Is the internal state empty (other than the output stream)?
     //! \returns `true` if no substrings are waiting to be assembled
     bool empty() const;
+
+    size_t num_written_bytes() const {return _num_written_bytes;}
 };
 
 #endif  // SPONGE_LIBSPONGE_STREAM_REASSEMBLER_HH
