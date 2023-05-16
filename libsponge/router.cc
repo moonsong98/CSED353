7ff3c7af02e098247044494e24c6202deb5d0800
Moon Kyung Song <moonsong98@postech.ac.kr>
2023-05-16 10:00:08 -0700

    Lab6: Passed All Tests

--- libsponge/router.cc
+++ libsponge/router.cc
@@ -26,17 +26,41 @@ void Router::add_route(const uint32_t route_prefix,
                        const uint8_t prefix_length,
                        const optional<Address> next_hop,
                        const size_t interface_num) {
-    cerr << "DEBUG: adding route " << Address::from_ipv4_numeric(route_prefix).ip() << "/" << int(prefix_length)
-         << " => " << (next_hop.has_value() ? next_hop->ip() : "(direct)") << " on interface " << interface_num << "\n";
-
-    DUMMY_CODE(route_prefix, prefix_length, next_hop, interface_num);
-    // Your code here.
+    _routing_table.push_back({route_prefix, prefix_length, next_hop, interface_num});
 }
 
 //! \param[in] dgram The datagram to be routed
 void Router::route_one_datagram(InternetDatagram &dgram) {
     DUMMY_CODE(dgram);
     // Your code here.
+    if (dgram.header().ttl <= 1)
+        return;
+
+    --dgram.header().ttl;
+
+    bool is_matched = false;
+    uint8_t matched_length = 0;
+    size_t interface_num = 0;
+    std::optional<Address> next_hop;
+
+    for (auto el : _routing_table) {
+        if ((not el.prefix_length or
+             (dgram.header().dst & (0xFFFFFFFF << (32 - el.prefix_length))) == el.route_prefix) and
+            matched_length <= el.prefix_length) {
+            is_matched = true;
+            matched_length = el.prefix_length;
+            interface_num = el.interface_num;
+            next_hop = el.next_hop;
+        }
+    }
+
+    if (not is_matched)
+        return;
+
+    if (next_hop.has_value())
+        interface(interface_num).send_datagram(dgram, next_hop.value());
+    else
+        interface(interface_num).send_datagram(dgram, Address::from_ipv4_numeric(dgram.header().dst));
 }
 
 void Router::route() {
--- libsponge/router.hh
+++ libsponge/router.hh
@@ -49,6 +49,15 @@ class Router {
     //! datagram's destination address.
     void route_one_datagram(InternetDatagram &dgram);
 
+    struct Route_Information {
+        const uint32_t route_prefix;
+        const uint8_t prefix_length;
+        const std::optional<Address> next_hop;
+        const size_t interface_num;
+    };
+
+    std::vector<Route_Information> _routing_table{};
+
   public:
     //! Add an interface to the router
     //! \param[in] interface an already-constructed network interface
